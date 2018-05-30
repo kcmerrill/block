8be01f30e12938f208672ebc40d61fc4831fb4fe
kc merrill <kcmerrill@gmail.com>
2018-05-30 17:28:07 -0600

    Cycling through inventory ...

--- block.go
+++ block.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
+
+	"github.com/kcmerrill/block/pkg/block"
 )
 
 func main() {
-	fmt.Println("hello block")
+	files := make(chan string)
+	finished := make(chan bool)
+	go block.Inventory("./", files, finished, []string{}, []string{})
+
+	for {
+		var done bool
+		select {
+		case <-time.After(1 * time.Second):
+			done = true
+		case done = <-finished:
+		case file := <-files:
+			fmt.Println(file)
+		}
+		if done {
+			break
+		}
+	}
 }
--- pkg/block/inventory.go
+++ pkg/block/inventory.go
@@ -1,12 +1,14 @@
 package block
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/karrick/godirwalk"
 )
 
-func filesystem(dir string) {
+// Inventory ...
+func Inventory(dir string, process chan string, finished chan bool, ignoreIfContains []string, ignoreIfStartsWith []string) {
 	// validate it exists
 	if _, exists := os.Stat(dir); exists != nil {
 		return
@@ -17,9 +19,13 @@ func filesystem(dir string) {
 		Unsorted: true,
 		Callback: func(osPathname string, de *godirwalk.Dirent) error {
 			if de.IsDir() {
+				fmt.Println(de.Name(), "is a directory")
+
 			} else {
+				fmt.Println(de.Name(), "is a file")
 			}
 			return nil
 		},
 	})
+	finished <- true
 }
